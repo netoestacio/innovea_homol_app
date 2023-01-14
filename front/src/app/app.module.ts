594031e90333e21425f35728396d19f49b902683
Pedro Estacio <pedronetoestacio@gmail.com>
2023-01-14 17:28:42 -0300

    routing and articleservice

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,8 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './component/article/article.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [
     ApiService
